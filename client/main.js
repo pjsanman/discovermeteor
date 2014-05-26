8634ef33b6d5491935b8ab804150d588ed1eefe9
Patrick Sanner <pj.sanner@gmail.com>
2014-05-26 03:13:35 +0000

    connected with intercom...

--- client/main.js
+++ client/main.js
@@ -1,1 +1,13 @@
 Meteor.subscribe('posts');
+
+
+// Intercom Settings
+  window.intercomSettings = {
+    // TODO: The current logged in user's full name
+    name: "John Doe",
+    // TODO: The current logged in user's email address.
+    email: "john.doe@example.com",
+    // TODO: The current logged in user's sign-up date as a Unix timestamp.
+    created_at: 1234567890,
+    app_id: "defb0a6f02dbc40ffbc423408555d8c033671095"
+  };
---
+++ packages/intercom/package.js
@@ -0,0 +1,7 @@
+Package.describe({
+  summary: "Intercom package"
+});
+
+Package.on_use(function (api) {
+  api.add_files('intercom_loader.js', 'client');
+});
