3fa857b9e242daa4785bd4257b699dbd4a9a7c6a
Patrick Sanner <pj.sanner@gmail.com>
2014-05-26 03:59:43 +0000

    fixed main.js

--- client/main.js
+++ client/main.js
@@ -9,6 +9,10 @@ Deps.autorun(function(){
       user_name: Meteor.user().username,
       user_id: Meteor.user()._id,
       user_hash: Meteor.user().intercomHash,
+      widget: {
+        activator: '#Intercom',
+        use_counter: true
+      },
       app_id: "defb0a6f02dbc40ffbc423408555d8c033671095"   
     };
     Intercom('boot', intercomSettings);
